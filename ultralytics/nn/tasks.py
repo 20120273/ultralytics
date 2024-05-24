e1a4a636ca6f178df5d9f9dd16c80394c5f8a2f8
20120273 <hongduong19042002@gmail.com>
2024-05-25 00:26:28 +0700

    up task

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -896,7 +896,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
             if m is SPD:
-                c2 = 4**c2
+                c2 = 4*c2
             if m is C2fAttn:
                 args[1] = make_divisible(min(args[1], max_channels // 2) * width, 8)  # embed channels
                 args[2] = int(
