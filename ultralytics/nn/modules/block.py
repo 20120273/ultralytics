aa60ef8973abe8d584d4789fe7ca0b3b0b691607
20120273 <hongduong19042002@gmail.com>
2024-05-25 10:41:10 +0700

    update file

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -13,7 +13,7 @@ __all__ = (
     "HGBlock",
     "HGStem",
     "SPP",
-    "SPD",
+    "space_to_depth",
     "SPPF",
     "SPPFCPSC",
     "C1",
@@ -41,7 +41,14 @@ __all__ = (
     "Silence",
 )
 
+class space_to_depth(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
 
+    def forward(self, x):
+         return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
 class DFL(nn.Module):
     """
     Integral module of Distribution Focal Loss (DFL).
